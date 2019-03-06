7853e07a78891f07820f66b3b4ffc2e1de377b41
Juuso Issakainen <juuso.issakainen@gmail.com>
2019-03-06 14:36:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,32 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3001;
 const weekend_url = 'https://en.wikipedia.org/wiki/2019_in_heavy_metal_music';
-const normal_url = 'https://en.wikipedia.org/wiki/Special:Random';
-const cartoon_url = 'https://xkcd.com/'
+const random_wiki_url = 'https://en.wikipedia.org/wiki/Special:Random';
+const xkcd_url = 'https://xkcd.com/';
+const what_if_url = 'https://what-if.xkcd.com/';
 
 const server = http.createServer((req, res) => {
-  var now = new Date();
-  if (now.getDay() < 1) {
-    var url = weekend_url;
+  var day = new Date().getDay();
+  switch (day) {
+    case 0:
+      var url = xkcd_url;
+      break;
+    case 1: 
+      var url = what_if_url;
+      break;
+    case 2:
+    case 3:
+      var url = random_wiki_url;
+      break;
+    case 4:
+    case 5:
+    case 6:
+      var url = weekend_url;
+      break;
+    default:
+      var url = 'http://www.google.fi';
   }
-  if (now.getDay() > 0) {
-    var url = cartoon_url;
-  }
-  else {
-    var url = normal_url;
-  }
-  res.writeHead(307,{Location: url});
+  res.writeHead(307,{'Location': url, 'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0-cache'});
   res.end();
 });
 
