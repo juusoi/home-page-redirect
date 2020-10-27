5106ee4bf51c11fd9641680c39b911304cbda762
Juuso Issakainen <juuso.issakainen@gmail.com>
2020-10-27 23:59:48 +0200

    logic check

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ const what_if_url = 'https://what-if.xkcd.com/';
 
 const server = http.createServer((req, res) => {
   switch (new Date().getDay()) {
-    case 0:
+    case 1: 
       var url = xkcd_url;
       break;
-    case 1: 
+    case 2:
       var url = what_if_url;
       break;
-    case 2:
     case 3:
       var url = random_wiki_url;
       break;
     case 4:
     case 5:
-    case 6:
       var url = weekend_url;
       break;
+    case 6:
+    case 0:
     default:
       var url = 'http://www.google.fi';
   }
