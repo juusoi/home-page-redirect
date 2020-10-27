5e3cc06807e28336225ba0ff8b1aaff3f92842bc
Juuso Issakainen <juuso.issakainen@gmail.com>
2020-10-28 00:57:35 +0200

    params

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = 'localhost';
 const port = 3001;
@@ -6,29 +7,36 @@ const weekend_url = 'https://en.wikipedia.org/wiki/2020_in_heavy_metal_music';
 const random_wiki_url = 'https://en.wikipedia.org/wiki/Special:Random';
 const xkcd_url = 'https://xkcd.com/';
 const what_if_url = 'https://what-if.xkcd.com/';
-const day = new Date().getDay();
+var day;
 
 const server = http.createServer((req, res) => {
+  const newDay = parseInt(url.parse(req.url,true).query.day);
+  //console.log(newDay);
+  if (isNaN(newDay)) {
+    day = new Date().getDay();
+  } else {
+    day = newDay;
+  }
   switch (day) {
     case 1: 
-      var url = xkcd_url;
+      var redirect_url = xkcd_url;
       break;
     case 2:
-      var url = what_if_url;
+      var redirect_url = what_if_url;
       break;
     case 3:
-      var url = random_wiki_url;
+      var redirect_url = random_wiki_url;
       break;
     case 4:
     case 5:
-      var url = weekend_url;
+      var redirect_url = weekend_url;
       break;
     case 6:
     case 0:
     default:
-      var url = 'http://www.google.fi';
+      var redirect_url = 'http://www.google.fi';
   }
-  res.writeHead(307,{'Location': url, 'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0-cache'});
+  res.writeHead(307,{'Location': redirect_url, 'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0-cache'});
   res.end();
 });
 
