422415482ee798e510aab9675da77f2e3beb17cf
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2021-11-22 23:49:14 +0200

    refactor and default to hs

--- app.js
+++ app.js
@@ -3,42 +3,37 @@ const url = require('url');
 
 const hostname = 'localhost';
 const port = 3001;
+
 const weekend_url = 'https://en.wikipedia.org/wiki/2021_in_heavy_metal_music';
 const random_wiki_url = 'https://en.wikipedia.org/wiki/Special:Random';
 const xkcd_url = 'https://xkcd.com/';
 const what_if_url = 'https://what-if.xkcd.com/';
-var day;
+const default_url = 'https://hs.fi'
 
 const server = http.createServer((req, res) => {
-  const newDay = parseInt(url.parse(req.url,true).query.day);
-  //console.log(newDay);
-  if (isNaN(newDay)) {
-    day = new Date().getDay();
-  } else {
-    day = newDay;
-  }
+  const newDay = parseInt(url.parse(req.url, true).query.day);
+  let day = (isNaN(newDay)) ? new Date().getDay() : newDay;
   switch (day) {
-    case 1: 
-      var redirect_url = xkcd_url;
-      break;
+    case 1:
+      redirect(res, xkcd_url);
     case 2:
-      var redirect_url = what_if_url;
-      break;
+      redirect(res, what_if_url);
     case 3:
-      var redirect_url = random_wiki_url;
-      break;
+      redirect(res, random_wiki_url);
     case 4:
     case 5:
-      var redirect_url = weekend_url;
-      break;
+      redirect(res, weekend_url);
     case 6:
     case 0:
     default:
-      var redirect_url = 'http://www.google.fi';
+      redirect(res, default_url);
   }
-  res.writeHead(307,{'Location': redirect_url, 'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0-cache'});
-  res.end();
 });
+
+function redirect(res, redirect_url) {
+  res.writeHead(307, { 'Location': redirect_url, 'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0-cache' });
+  res.end();
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
