ccf1d0c6f8f71d574fd22224d75e5b6c289a3273
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-04-19 14:26:10 +0300

    ci

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const http = require('http');
 const { server } = require('./server');
 
 const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 3001;
 
-const app = http.createServer(server);
-
-app.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
